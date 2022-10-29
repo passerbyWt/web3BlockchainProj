2973c3a7cb0ca064fcd86f53ae86b1ce3872f85d
adamlin859 <ycl2112@columbia.edu>
2022-10-29 15:46:25 -0400

    added lottery data structure and part of start lottery

--- CovidLotteryTracker.sol
+++ CovidLotteryTracker.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// contract inspired from https://github.com/pancakeswap/lottery-contract/tree/master/contracts
+
 
 interface RandomNumGenerator {
     /**
@@ -24,15 +26,33 @@ interface RandomNumGenerator {
 contract CovidLotteryTracker {
     using SafeERC20 for IERC20;
     
+    enum Status {
+        Pending, 
+        Open, 
+        Close
+    }
+    
     struct responder {
         bool result;
         bool responded;
     }
 
+    struct Lottery {
+        Status status;
+    }
+
     IERC20 public lottoToken;
     RandomNumGenerator public randomGen;
     address[] public lotteryPool;
+    mapping(uint256 => Lottery) private _lotteries;
+
     mapping(address => responder) public responses;
+
+    // need a way to allow users to define the time which the lotter start or stops
+    uint256 public constant MIN_LENGTH_LOTTERY = 2 minutes;
+    uint256 public constant MAX_LENGTH_LOTTERY = 2 minutes;
+    uint256 public constant MAX_TREASURY_FEE = 300; // 3%
+
 
     
     constructor(address _lottoTokenAddress, address _randomGeneratorAddress) {
@@ -40,8 +60,9 @@ contract CovidLotteryTracker {
         randomGen  = RandomNumGenerator(_randomGeneratorAddress);
     }
     
-
-    function gotCovidCondition(bool res) public {
+    // need to take care of other conditions, maybe different way to enter 
+    // into the lottery
+    function pushCovidCondition(bool res) public {
         require(
             !responses[msg.sender].responded,
             "the person already responded"
@@ -52,6 +73,19 @@ contract CovidLotteryTracker {
 
         lotteryPool.push(msg.sender);
     }
+
+    function startLottery(uint256 _endTime, uint256 _treasuryFee) {
+        require(
+            ((_endTime - block.timestamp) > MIN_LENGTH_LOTTERY) && ((_endTime - block.timestamp) < MAX_LENGTH_LOTTERY), 
+            "Lottery length outside of range"
+        );
+
+
+    }
+
+    // function drawFinalWinner() {
+        
+    // }
 
 
 
