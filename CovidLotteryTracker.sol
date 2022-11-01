46dad74b9e050902c71cab3890e7614ab3fde10a
adamlin859 <ycl2112@columbia.edu>
2022-11-01 01:02:31 -0400

    Complete stratLottery method

--- CovidLotteryTracker.sol
+++ CovidLotteryTracker.sol
@@ -39,12 +39,16 @@ contract CovidLotteryTracker {
 
     struct Lottery {
         Status status;
+        uint startingLotteryAmt;
+        uint256 startTime;
+        uint256 endTime;
+        mapping(uint256 => address[]) lotteryPool;
     }
 
     IERC20 public lottoToken;
     RandomNumGenerator public randomGen;
-    address[] public lotteryPool;
     mapping(uint256 => Lottery) private _lotteries;
+    uint256 public currentLotteryId;
 
     mapping(address => responder) public responses;
 
@@ -54,6 +58,15 @@ contract CovidLotteryTracker {
     uint256 public constant MAX_TREASURY_FEE = 300; // 3%
 
 
+    // data to store on the blockchain
+    event LotteryOpen(
+        uint256 indexed lotteyID,
+        uint256 startingLotteryAmt,
+        uint256 startTime,
+        uint256 endTime
+
+    );
+
     
     constructor(address _lottoTokenAddress, address _randomGeneratorAddress) {
         lottoToken = IERC20(_lottoTokenAddress);
@@ -62,23 +75,42 @@ contract CovidLotteryTracker {
     
     // need to take care of other conditions, maybe different way to enter 
     // into the lottery
-    function pushCovidCondition(bool res) public {
+    function pushCovidCondition(bool res, uint lotteryID) public {
         require(
             !responses[msg.sender].responded,
             "the person already responded"
         );
         
+
+        // currently just storing one response, but we can add more to this later
         responses[msg.sender].responded = true;
         responses[msg.sender].result = res;
 
-        lotteryPool.push(msg.sender);
+        // create the lottery pool for the lotter. 
+        _lotteries[lotteryID].lotteryPool.push(msg.sender);
     }
 
-    function startLottery(uint256 _endTime, uint256 _treasuryFee) {
+    function startLottery(uint256 _startingLotteryAmt, uint256 _endTime, uint256 _treasuryFee) {
         require(
             ((_endTime - block.timestamp) > MIN_LENGTH_LOTTERY) && ((_endTime - block.timestamp) < MAX_LENGTH_LOTTERY), 
             "Lottery length outside of range"
         );
+        
+        require(_treasuryFee <= MAX_TREASURY_FEE, "Treasury fee too high.");
+
+        currentLotteryId++;
+        _lotteries[currentLotteryId] = Lottery({
+            status: Status.Open,
+            startingLotteryAmt: _startingLotteryAmt,
+            startTime: block.timestamp,
+            endTime: _endTime
+        });
+
+        emit LotteryOpen(currentLotteryId, block.timestamp, _endTime, _startingLotteryAmt);
+
+
+
+
 
 
     }
