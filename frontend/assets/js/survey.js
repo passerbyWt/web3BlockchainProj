32a829db8576888876ae898c103ae152243d6a86
adamlin859 <ycl2112@columbia.edu>
2022-12-05 17:33:23 -0500

    added web3

--- frontend/assets/js/survey.js
+++ frontend/assets/js/survey.js
@@ -1,1 +1,34 @@
 let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
+web3.eth.defaultAccount = "0x3F841f06A5e6CeF72450cE1303b25CBC76A97Ed1";
+const test_address = "0x9f27f5791D5031cA89d0CA6D59276659EAD06242"
+const test_abi = [
+	{
+		"inputs": [],
+		"name": "add",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	}
+];
+
+const test_contract = new web3.eth.Contract(test_abi, test_address);
+
+async function start() {
+    var added_val = await test_contract.methods.add().call({from:web3.eth.defaultAccount});
+    console.log(added_val);
+}
+
+
+start();
+
+web3.eth.getBalance(web3.eth.defaultAccount).then((response) => {
+    $("#csx-token").html(response);
+});
+
